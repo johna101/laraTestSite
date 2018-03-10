91a38f2dd4d0e63939ab9958b936adf19979a2d5
John Anderson <john@maclarenwest.co.uk>
2018-03-10 23:48:42 +0000

    Database connection

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'sqlsrv'),
 
     /*
     |--------------------------------------------------------------------------
@@ -69,11 +69,11 @@ return [
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'snd-dbserver.database.windows.net'),
             'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'laravelTestSite'),
+            'username' => env('DB_USERNAME', 'adminuser'),
+            'password' => env('DB_PASSWORD', 'Poi098**'),
             'charset' => 'utf8',
             'prefix' => '',
         ],
